eb03763b911c14ec9b77ce32049fc9b8b4f7c478
T <endlesstruffle80@gmail.com>
2022-10-14 10:40:09 -0700

    Adds abiltiy to get manager information and send array of manager objects to have a profile generatedfor each manager object.

--- index.js
+++ index.js
@@ -2,31 +2,19 @@ const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const inquirer = require('inquirer');
+const {startFile, generateManagerProfile, generateEngineerProfile, generateInternProfile, finishFile} = require('./src/generateTeamProfile');
 
 const managers = [];
 const engineers = [];
 const interns = [];
-let position = 'employee';
-const questions = [{
-    type: 'input',
-    name: 'name',
-    message: `Please enter the ${position}'s name:`,
-}, {
-    type: 'input',
-    name: 'id',
-    message: `Please enter the ${position}'s id:`,
-}, {
-    type: 'input',
-    name: 'email',
-    message: `Please enter the ${position}'s email:`
-}]
 
 function init() {
 
-    console.log('Welcome!')
+    console.log();
+    console.log('Welcome!');
     console.log(`This application will: 
 - Collect information about your team
-- Generate an HTML document to display your team's profile`);
+- Generate an HTML document to display your team's profile\n`);
 
     const initPrompt = inquirer.createPromptModule();
     initPrompt({
@@ -36,7 +24,7 @@ function init() {
     })
     .then((answers) => {
         if(answers.continue) {
-            new inquirer.Separator();
+            console.log('=============================================================\n')
             getManagerInformation();
         } else {
             console.clear();
@@ -45,6 +33,93 @@ function init() {
     });
 }
 
+function getManagerInformation() {
+    startFile();
 
+    const managerQs = getQuestions("Manager",[{
+        type: 'input',
+        name: 'officeNum',
+        message: `Please enter the Manager's office number:`
+    }]);
+    
+    inquirer.prompt(managerQs)
+    .then((answers) => {
+
+        managers.push(answers);
+
+        if(answers.continue) {
+
+            getManagerInformation();
+        } else {
+
+            //*generateManagerProfile(managers);
+            const wantEngineer = wantNextRole('Engineer');
+
+            if(wantEngineer) {
+                getEngineerInforamtion();
+            } else {
+
+                const wantIntern = wantNextRole('Intern');
+
+                if(wantIntern) {
+                    getInternInformation();
+                } else {
+                    finishFile();
+                }
+            }
+        }
+    })
+    .catch(console.error);
+}
+
+function getQuestions(position, objArr) {
+
+    const questions = [{
+        type: 'input',
+        name: 'name',
+        message: `Please enter the ${position}'s name:`,
+    }, {
+        type: 'input',
+        name: 'id',
+        message: `Please enter the ${position}'s id:`,
+    }, {
+        type: 'input',
+        name: 'email',
+        message: `Please enter the ${position}'s email:`
+    }]
+
+    objArr.forEach((obj) => {questions.push(obj)});
+    questions.push(getRepeatQ(position));
+
+    return questions;
+}
+
+function getRepeatQ(position) {
+
+    const repeatPrompt = {
+        type: 'confirm',
+        name: 'continue',
+        message: `Would you like to add another ${position}?`,
+    }
+
+    return repeatPrompt;
+}
+
+function wantNextRole(position) {
+
+    const rolePrompt = inquirer.createPromptModule();
+            rolePrompt({
+                type: 'confirm',
+                name: 'continue',
+                message: `Would you like to generate a profile for an ${position}?`
+            })
+            .then((answers) => {
+                if(answers.continue) {
+                    return true
+                } else {
+                    return false
+                }
+            });
+}
 
 init();
--- src/generateTeamProfile.js
+++ src/generateTeamProfile.js
@@ -1,4 +1,19 @@
+const fs = require('fs');
 
+
+
+function generateManagerProfile(managers) {
+    let managerProfile;
+
+    managers.forEach(manager => {
+        managerProfile = getManagerProfile(manager);
+        let managerStr  = JSON.stringify(managerProfile,null,2);
+        fs.appendFile('../dist/teamProfile.html', managerStr,(err) => {
+            err ? console.error(err) : console.log("Manager profile sucessfully added!");
+        })
+    });
+
+}
 
 
 
